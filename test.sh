a61522a182f3caab6c3f578f21e5519cba53e444
MIAOBO HU <wandugu@126.com>
2025-09-05 21:57:06 +0800

    feat: allow selecting GPU via argument in train and test scripts

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Evaluate the model on the test set using a pretrained checkpoint without training.
-# Usage: bash test.sh [path_to_checkpoint]
+# Usage: bash test.sh [GPU_ID] [path_to_checkpoint]
 
+GPU_ID=${1:-0}
+shift
 MODEL_PATH=${1:-ckpt/best_model.pt}
-python main.py --encoder_v resnet101 --gate --load_model "$MODEL_PATH"
+CUDA_VISIBLE_DEVICES=$GPU_ID python main.py --encoder_v resnet101 --gate --load_model "$MODEL_PATH"
+
